d2c0116d5842934f9d7b1dc89e50043248bac1c2
Chris Bojemski <chris.bojemski@gmail.com>
2018-10-24 10:19:29 -0400

    Find or create CalDate objects.

--- app/controllers/api/v1/calendar_dates_controller.rb
+++ app/controllers/api/v1/calendar_dates_controller.rb
@@ -6,11 +6,11 @@ class Api::V1::CalendarDatesController < ApplicationController
   end
 
   def create
-    @calendar_date = CalendarDate.new(event_params)
+    @calendar_date = CalendarDate.find_or_create_by(calendar_date_params)
     if @calendar_date.save
       render json: @calendar_date, status: :accepted
     else
-      render json: { errors: 'Event not accepted.' }, status: :unprocessible_entity
+      render json: { errors: 'CalDate not accepted.' }, status: :unprocessible_entity
     end
   end
 
