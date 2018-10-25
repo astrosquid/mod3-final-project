05e87d0c69be48e646eaf69b128be6e9fb94698c
danmj <daniel.jung@stern.nyu.edu>
2018-10-25 14:00:25 -0400

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+CalendarDate.create(date: 'October 24, 2018')
+
+Event.create(title: "Dinner with Grandma", location: "Grandma's", calendar_date_id: 1)
