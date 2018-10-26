28cbc52dcee622e5d855a7a50e4d6d9d2bb1527b
Chris Bojemski <chris.bojemski@gmail.com>
2018-10-26 10:54:30 -0400

    Extra items in seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
 CalendarDate.create(date: 'October 24, 2018')
+CalendarDate.create(date: 'October 10, 2018')
 
 Event.create(title: "Dinner with Grandma", location: "Grandma's", calendar_date_id: 1)
+Event.create(title: "Go to Flatiron", location: "11 Broadway", calendar_date_id: 2)
+
