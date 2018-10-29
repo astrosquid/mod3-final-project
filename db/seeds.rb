95069c7a65321123413127b4a6906ce77b97c27c
Chris Bojemski <chris.bojemski@gmail.com>
2018-10-28 21:53:20 -0400

    Adding seed print lines.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
+puts "Adding CalendarDates."
 CalendarDate.create(date: 'October 24, 2018')
 CalendarDate.create(date: 'October 10, 2018')
 
+puts "Adding Events."
 Event.create(title: "Dinner with Grandma", location: "Grandma's", calendar_date_id: 1)
 Event.create(title: "Go to Flatiron", location: "11 Broadway", calendar_date_id: 2)
-
